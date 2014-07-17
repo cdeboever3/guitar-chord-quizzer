5affd81135f0bf299db6e0639878ae420623ef5c
Christopher DeBoever <cdeboever3@gmail.com>
2014-07-17 10:38:47 -0700

    Added some initial files
    Just starting to work on Quizzer class.

---
+++ guitar-chord-quizzer/classes.py
@@ -0,0 +1,12 @@
+import sys
+
+class Quizzer:
+    def __init__(self):
+        self.alive = True
+        sys.stdout.write('Enter the number of the chord set you\'d like to'
+                         'practice.\n')
+        # show chord sets and number them
+        # gather input, check if correct, proceed
+
+        
+        self.quizzing = True
