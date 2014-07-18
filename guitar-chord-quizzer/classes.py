4b36cf1215dfcb1c3a8026b4b135800e8d407be2
Christopher DeBoever <cdeboever3@gmail.com>
2014-07-17 17:41:16 -0700

    Small updates

--- guitar-chord-quizzer/classes.py
+++ guitar-chord-quizzer/classes.py
@@ -1,12 +1,24 @@
+import os
 import sys
+
+class ChordSet:
+    def __init__(self, fn):
+        """
+        Initialize ChordSet object from file fn
+        """
+        self.name = os.path.splitext(fn)[0]
+        with open(fn) as f:
+            lines = [ x.strip() for x in f.readlines() ]
+        self.chords = lines            
 
 class Quizzer:
     def __init__(self):
         self.alive = True
-        sys.stdout.write('Enter the number of the chord set you\'d like to'
-                         'practice.\n')
+        string = 'Enter the number of the chord set you\'d like to practice.\n'
+        raw_input(string)
         # show chord sets and number them
         # gather input, check if correct, proceed
+        raw_input
 
         
         self.quizzing = True
