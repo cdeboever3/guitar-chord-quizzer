2382cd0104650aab68123bccd6e0335ddeae52f3
Christopher DeBoever <cdeboever3@gmail.com>
2014-07-17 18:29:59 -0700

    Choosing chord set at command line working

--- guitar-chord-quizzer/classes.py
+++ guitar-chord-quizzer/classes.py
@@ -1,4 +1,6 @@
+import glob
 import os
+import pdb
 import sys
 
 class ChordSet:
@@ -6,19 +8,37 @@ class ChordSet:
         """
         Initialize ChordSet object from file fn
         """
-        self.name = os.path.splitext(fn)[0]
+        self.name = os.path.splitext(os.path.split(fn)[1])[0]
         with open(fn) as f:
             lines = [ x.strip() for x in f.readlines() ]
         self.chords = lines            
 
 class Quizzer:
     def __init__(self):
-        self.alive = True
-        string = 'Enter the number of the chord set you\'d like to practice.\n'
-        raw_input(string)
-        # show chord sets and number them
-        # gather input, check if correct, proceed
-        raw_input
-
-        
+        self.chord_sets = self.make_chord_sets()
+        self.current_chord_set = self.get_chord_set_from_user()
         self.quizzing = True
+
+    def get_chord_set_from_user(self):
+        string = ('Enter the number of the chord set you\'d like to practice.'
+                  '\n\n')
+        for i, v in enumerate(self.chord_sets):
+            string += '{}: {}\n'.format(i + 1, v.name)
+        string += '\n'
+        while True:
+            current_chord_set = raw_input(string)
+            try:
+                index = int(current_chord_set) - 1
+                assert index >= 0
+                assert index < len(self.chord_sets)
+                break
+            except (ValueError, AssertionError):
+                sys.stdout.write('You didn\'t enter a valid number, try '
+                                 'again.\n\n')
+        return self.chord_sets[index]
+
+    def make_chord_sets(self):
+        root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        chords_files = glob.glob(os.path.join(root, 'chords', '*'))
+        chord_sets = [ChordSet(fn) for fn in chords_files]
+        return chord_sets
